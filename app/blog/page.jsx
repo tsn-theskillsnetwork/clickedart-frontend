a80265a0ebeb38553a82311db45ba2be58a9720f
bhanu-sh <bhanu1234sharma@gmail.com>
2024-12-30 18:55:45 +0530

    Success Story Section in blogs page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,165 +1,35 @@
 "use client";
 
+import { fetchData } from "@/helpers/api";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function BlogPage() {
   const [blogLength, setBlogLength] = useState(6);
-  const blogPosts = [
-    {
-      authorInfo: {
-        author: {
-          name: "John Doe",
-          avatar: "/assets/images/avatar.jpg",
-        },
-        authorType: "Author",
-      },
-      slug: "from-ideas-to-masterpieces",
-      content: {
-        title: "From Ideas to Masterpieces Heading Lorem Ipsum",
-        summary:
-          "Inspiration, tips, and stories to bring your creative vision to life. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt velit ullam omnis nam praesentium officiis.",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-      },
-      coverImage: "/assets/images/img1.jpg",
-      tags: ["Art & Design"],
-      date: "2024-12-24",
-    },
-    {
-      authorInfo: {
-        author: {
-          name: "John Doe",
-          avatar: "/assets/images/avatar.jpg",
-        },
-        authorType: "Author",
-      },
-      slug: "from-ideas-to-masterpieces",
-      content: {
-        title: "From Ideas to Masterpieces Heading Lorem Ipsum",
-        summary:
-          "Inspiration, tips, and stories to bring your creative vision to life. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt velit ullam omnis nam praesentium officiis.",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-      },
-      coverImage: "/assets/images/img2.jpg",
-      tags: ["Art & Design"],
-      date: "2024-12-24",
-    },
-    {
-      authorInfo: {
-        author: {
-          name: "John Doe",
-          avatar: "/assets/images/avatar.jpg",
-        },
-        authorType: "Author",
-      },
-      slug: "from-ideas-to-masterpieces",
-      content: {
-        title: "From Ideas to Masterpieces Heading Lorem Ipsum",
-        summary:
-          "Inspiration, tips, and stories to bring your creative vision to life. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt velit ullam omnis nam praesentium officiis.",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-      },
-      coverImage: "/assets/images/img3.jpg",
-      tags: ["Art & Design"],
-      date: "2024-12-24",
-    },
-    {
-      authorInfo: {
-        author: {
-          name: "John Doe",
-          avatar: "/assets/images/avatar.jpg",
-        },
-        authorType: "Author",
-      },
-      slug: "from-ideas-to-masterpieces",
-      content: {
-        title: "From Ideas to Masterpieces Heading Lorem Ipsum",
-        summary:
-          "Inspiration, tips, and stories to bring your creative vision to life. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt velit ullam omnis nam praesentium officiis.",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-      },
-      coverImage: "/assets/images/img4.jpg",
-      tags: ["Art & Design"],
-      date: "2024-12-24",
-    },
-    {
-      authorInfo: {
-        author: {
-          name: "John Doe",
-          avatar: "/assets/images/avatar.jpg",
-        },
-        authorType: "Author",
-      },
-      slug: "from-ideas-to-masterpieces",
-      content: {
-        title: "From Ideas to Masterpieces Heading Lorem Ipsum",
-        summary:
-          "Inspiration, tips, and stories to bring your creative vision to life. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt velit ullam omnis nam praesentium officiis.",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-      },
-      coverImage: "/assets/images/img5.jpg",
-      tags: ["Art & Design"],
-      date: "2024-12-24",
-    },
-    {
-      authorInfo: {
-        author: {
-          name: "John Doe",
-          avatar: "/assets/images/avatar.jpg",
-        },
-        authorType: "Author",
-      },
-      slug: "from-ideas-to-masterpieces",
-      content: {
-        title: "From Ideas to Masterpieces Heading Lorem Ipsum",
-        summary:
-          "Inspiration, tips, and stories to bring your creative vision to life. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt velit ullam omnis nam praesentium officiis.",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-      },
-      coverImage: "/assets/images/img6.jpg",
-      tags: ["Art & Design"],
-      date: "2024-12-24",
-    },
-    {
-      authorInfo: {
-        author: {
-          name: "John Doe",
-          avatar: "/assets/images/avatar.jpg",
-        },
-        authorType: "Author",
-      },
-      slug: "from-ideas-to-masterpieces",
-      content: {
-        title: "From Ideas to Masterpieces Heading Lorem Ipsum",
-        summary:
-          "Inspiration, tips, and stories to bring your creative vision to life. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt velit ullam omnis nam praesentium officiis.",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-      },
-      coverImage: "/assets/images/img2.jpg",
-      tags: ["Art & Design"],
-      date: "2024-12-24",
-    },
-    {
-      authorInfo: {
-        author: {
-          name: "John Doe",
-          avatar: "/assets/images/avatar.jpg",
-        },
-        authorType: "Author",
-      },
-      slug: "from-ideas-to-masterpieces",
-      content: {
-        title: "From Ideas to Masterpieces Heading Lorem Ipsum",
-        summary:
-          "Inspiration, tips, and stories to bring your creative vision to life. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt velit ullam omnis nam praesentium officiis.",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-      },
-      coverImage: "/assets/images/img3.jpg",
-      tags: ["Art & Design"],
-      date: "2024-12-24",
-    },
-  ];
+  const [blogPosts, setBlogPosts] = useState([]);
+  const [successStories, setSuccessStories] = useState([]);
+
+  console.log(blogPosts);
+  console.log(successStories);
+
+  useEffect(() => {
+    fetchData(
+      `${process.env.NEXT_PUBLIC_SERVER}/api/blog/get-all-blogs`,
+      "blogs",
+      setBlogPosts,
+      null,
+      null
+    );
+    fetchData(
+      `${process.env.NEXT_PUBLIC_SERVER}/api/blog/get-all-success-stories`,
+      "successstories",
+      setSuccessStories,
+      null,
+      null
+    );
+  }, []);
+
   return (
     <div className="flex flex-col px-4 lg:px-20">
       <div className="relative flex flex-col my-5 sm:my-10 md:my-16 xl:my-20 rounded-2xl overflow-hidden">
@@ -181,7 +51,69 @@ export default function BlogPage() {
           </p>
         </div>
       </div>
+      {successStories?.length > 0 && (
+        <>
+          <hr className="mb-5 border border-primary-100" />
+          <h2 className="text-heading-04 sm:text-heading-03 lg:text-heading-02 font-bold mb-5">
+            Success Stories
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10 mb-20">
+            {successStories.slice(0, blogLength).map((post, index) => (
+              <Link
+                key={index}
+                href={`/blog/${post.slug}`}
+                className="flex flex-col gap-4"
+              >
+                <Image
+                  src={post.coverImage[0] || "/assets/default.jpg"}
+                  alt={post.content.title || "Blog Post"}
+                  width={600}
+                  height={400}
+                  className="w-full aspect-[16/9] rounded-lg object-cover"
+                />
+
+                <div className="flex gap-2 flex-wrap">
+                  {post.tags.map((tag, index) => (
+                    <span
+                      key={index}
+                      className="text-sm sm:text-base md:text-heading-06 lg:text-paragraph font-medium text-surface-700 bg-primary-400 bg-opacity-15 rounded-lg px-2 py-1"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <div className="flex flex-col gap-2">
+                  <h5 className="text-heading-06 sm:text-heading-05 lg:text-heading-04 font-semibold">
+                    {post.content.title}
+                  </h5>
+                  <div className="flex items-center gap-2">
+                    <div className="flex gap-2 text-sm md:text-base text-surface-500">
+                      <p className="font-bold border-r-2 border-[#7777778f] pr-2">
+                        {post.authorInfo.author.name}
+                      </p>
+                      <p className="">
+                        {" "}
+                        {new Date(post.createdAt).toLocaleDateString("en-US", {
+                          month: "short",
+                          day: "numeric",
+                          year: "numeric",
+                        })}
+                      </p>
+                    </div>
+                  </div>
+                  <p className="text-sm sm:text-base md:text-heading-06 lg:text-paragraph font-medium text-surface-600">
+                    {post.content.summary}
+                  </p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
       <hr className="mb-5 border border-primary-100" />
+      <h2 className="text-heading-04 sm:text-heading-03 lg:text-heading-02 font-bold mb-5">
+        Blogs
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10 mb-20">
         {blogPosts.slice(0, blogLength).map((post, index) => (
           <Link
@@ -190,14 +122,14 @@ export default function BlogPage() {
             className="flex flex-col gap-4"
           >
             <Image
-              src={post.coverImage}
+              src={post.coverImage[0] || "/assets/default.jpg"}
               alt={post.content.title || "Blog Post"}
               width={600}
               height={400}
               className="w-full aspect-[16/9] rounded-lg object-cover"
             />
 
-            <p>
+            <div className="flex gap-2 flex-wrap">
               {post.tags.map((tag, index) => (
                 <span
                   key={index}
@@ -206,7 +138,7 @@ export default function BlogPage() {
                   {tag}
                 </span>
               ))}
-            </p>
+            </div>
             <div className="flex flex-col gap-2">
               <h5 className="text-heading-06 sm:text-heading-05 lg:text-heading-04 font-semibold">
                 {post.content.title}
@@ -216,7 +148,14 @@ export default function BlogPage() {
                   <p className="font-bold border-r-2 border-[#7777778f] pr-2">
                     {post.authorInfo.author.name}
                   </p>
-                  <p className=""> {post.date}</p>
+                  <p className="">
+                    {" "}
+                    {new Date(post.createdAt).toLocaleDateString("en-US", {
+                      month: "short",
+                      day: "numeric",
+                      year: "numeric",
+                    })}
+                  </p>
                 </div>
               </div>
               <p className="text-sm sm:text-base md:text-heading-06 lg:text-paragraph font-medium text-surface-600">
