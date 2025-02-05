920665ff355cca5a2e33857371dcb6a71189e0e1
bhanu-sh <bhanu1234sharma@gmail.com>
2025-02-05 14:23:43 +0530

    feat: implement blog creation page with image upload and validation

---
+++ app/profile/blog/edit/[id]/page.jsx
@@ -0,0 +1,308 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useMemo, useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import toast from "react-hot-toast";
+import axios from "axios";
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
+import Link from "next/link";
+import useAuthStore from "@/authStore";
+import Loader from "@/components/loader";
+import Swal from "sweetalert2";
+
+// Dynamic import for ReactQuill to avoid SSR issues
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+
+export default function BlogEdit() {
+  const { id } = useParams();
+  const { token, photographer, isHydrated } = useAuthStore();
+  const router = useRouter();
+  const [error, setError] = useState(null);
+  const [blog, setBlog] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          ["bold", "italic", "underline", "strike"],
+          [
+            { align: "" },
+            { align: "center" },
+            { align: "right" },
+            { align: "justify" },
+          ],
+          [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" },
+          ],
+          [
+            {
+              color: [
+                "#000000",
+                "#e60000",
+                "#ff9900",
+                "#ffff00",
+                "#008a00",
+                "#0066cc",
+                "#9933ff",
+                "#ffffff",
+                "#facccc",
+                "#ffebcc",
+                "#ffffcc",
+                "#cce8cc",
+                "#cce0f5",
+                "#ebd6ff",
+                "#bbbbbb",
+                "#f06666",
+                "#ffc266",
+                "#ffff66",
+                "#66b966",
+                "#66a3e0",
+                "#c285ff",
+                "#888888",
+                "#a10000",
+                "#b26b00",
+                "#b2b200",
+                "#006100",
+                "#0047b2",
+                "#6b24b2",
+                "#444444",
+                "#5c0000",
+                "#663d00",
+                "#666600",
+                "#003700",
+                "#002966",
+                "#3d1466",
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+    []
+  );
+
+  const validateForm = () => {
+    if (!blog.content.title.trim()) {
+      toast.error("Title is required.");
+      return false;
+    }
+    if (!blog.content.body.trim()) {
+      toast.error("Body is required.");
+      return false;
+    }
+    return true;
+  };
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const uploadData = new FormData();
+    uploadData.append("image", file);
+
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER}/api/upload/uploadSingleImage`,
+        {
+          method: "POST",
+          body: uploadData,
+        }
+      );
+      const data = await res.text();
+      if (res.ok) {
+        setBlog((prev) => ({
+          ...prev,
+          coverImage: data,
+        }));
+        toast.success("Image uploaded successfully!");
+      } else {
+        toast.error("Image upload failed.");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("An error occurred while uploading the image.");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER}/api/blog/update-blog`,
+        blog,
+        {
+          headers: {
+            "x-auth-token": token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.data;
+      if (data.error) {
+        return toast.error(data.error);
+      }
+      Swal.fire({
+        title: "Blog Updated",
+        text: "Your blog has been updated successfully.",
+        icon: "success",
+        confirmButtonText: "OK",
+      }).then(() => {
+        router.push(`/profile/blog`);
+      });
+    } catch (error) {
+      console.error(error);
+      setError(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_SERVER}/api/blog/get-blog-by-id?id=${id}`
+        );
+        const data = response.data;
+
+        // setBlog(data.blog);
+        setBlog({ ...data.blog, blogId: data.blog._id });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (photographer) {
+      fetchBlogs();
+    }
+  }, [photographer]);
+
+  return (
+    <div className="p-4">
+      {!isLoading && blog ? (
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-4xl mx-auto mt-5 min-h-screen"
+        >
+          <div className="bg-white shadow-[0_0_8px_rgba(0,0,0,0.3)] rounded-lg p-6 space-y-6">
+            <div>
+              <Label htmlFor="coverImage">Cover Image</Label>
+              <Input
+                id="coverImage"
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+              />
+              {blog.coverImage && (
+                <img
+                  src={blog.coverImage}
+                  alt="Cover Preview"
+                  className="mt-4 w-full h-48 object-contain rounded-lg shadow-sm"
+                />
+              )}
+            </div>
+            <div>
+              <Label htmlFor="title">Title</Label>
+              <Input
+                id="title"
+                type="text"
+                value={blog.content?.title || ""}
+                onChange={(e) =>
+                  setBlog({
+                    ...blog,
+                    content: { ...blog.content, title: e.target.value },
+                  })
+                }
+              />
+            </div>
+            <div>
+              <Label htmlFor="summary">Summary</Label>
+              <Textarea
+                id="summary"
+                value={blog.content?.summary || ""}
+                onChange={(e) =>
+                  setBlog({
+                    ...blog,
+                    content: { ...blog.content, summary: e.target.value },
+                  })
+                }
+              />
+            </div>
+            <div>
+              <Label htmlFor="body">Body</Label>
+              <ReactQuill
+                modules={modules}
+                theme="snow"
+                value={blog.content?.body || ""}
+                onChange={(value) =>
+                  setBlog({
+                    ...blog,
+                    content: { ...blog.content, body: value },
+                  })
+                }
+              />
+            </div>
+            <div>
+              <Label htmlFor="tags">Tags</Label>
+              <Input
+                id="tags"
+                type="text"
+                value={blog.tags?.join(", ") || ""}
+                onChange={(e) =>
+                  setBlog({
+                    ...blog,
+                    tags: e.target.value.split(",").map((tag) => tag.trim()),
+                  })
+                }
+              />
+            </div>
+            <hr />
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+            <Button
+              variant="primary"
+              type="submit"
+              className="w-full bg-blue-600 text-white hover:bg-blue-700"
+            >
+              Update Blog
+            </Button>
+          </div>
+        </form>
+      ) : (
+        <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
+          {isHydrated ? (
+            <>
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <>
+                  {!blog ? (
+                    <h1 className="text-3xl font-bold text-center text-gray-800">
+                      No Blog Found
+                    </h1>
+                  ) : (
+                    <h1 className="text-3xl font-bold text-center text-gray-800">
+                      You need to be a photographer to create a blog.
+                    </h1>
+                  )}
+                </>
+              )}
+            </>
+          ) : (
+            <Loader />
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
--- app/profile/blog/page.jsx
+++ app/profile/blog/page.jsx
@@ -8,287 +8,165 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import toast from "react-hot-toast";
 import axios from "axios";
-import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
 import Link from "next/link";
 import useAuthStore from "@/authStore";
 import Loader from "@/components/loader";
 import Swal from "sweetalert2";
-
-// Dynamic import for ReactQuill to avoid SSR issues
-const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+import { Trash } from "lucide-react";
+import Button2 from "@/components/button2";
 
 export default function BlogCreate() {
   const { token, photographer, isHydrated } = useAuthStore();
   const router = useRouter();
   const [error, setError] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const modules = useMemo(
-    () => ({
-      toolbar: {
-        container: [
-          [{ header: [1, 2, 3, 4, 5, 6, false] }],
-          ["bold", "italic", "underline", "strike"],
-          [
-            { align: "" },
-            { align: "center" },
-            { align: "right" },
-            { align: "justify" },
-          ],
-          [
-            { list: "ordered" },
-            { list: "bullet" },
-            { indent: "-1" },
-            { indent: "+1" },
-          ],
-          [
+  const handleDelete = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3e3e3e",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await axios.delete(
+            `${process.env.NEXT_PUBLIC_SERVER}/api/blog/delete-blog?blogId=${id}`,
             {
-              color: [
-                "#000000",
-                "#e60000",
-                "#ff9900",
-                "#ffff00",
-                "#008a00",
-                "#0066cc",
-                "#9933ff",
-                "#ffffff",
-                "#facccc",
-                "#ffebcc",
-                "#ffffcc",
-                "#cce8cc",
-                "#cce0f5",
-                "#ebd6ff",
-                "#bbbbbb",
-                "#f06666",
-                "#ffc266",
-                "#ffff66",
-                "#66b966",
-                "#66a3e0",
-                "#c285ff",
-                "#888888",
-                "#a10000",
-                "#b26b00",
-                "#b2b200",
-                "#006100",
-                "#0047b2",
-                "#6b24b2",
-                "#444444",
-                "#5c0000",
-                "#663d00",
-                "#666600",
-                "#003700",
-                "#002966",
-                "#3d1466",
-              ],
-            },
-          ],
-        ],
-      },
-    }),
-    []
-  );
-
-  const [blog, setBlog] = useState({
-    authorInfo: {
-      author: undefined,
-      authorType: "Photographer",
-    },
-    slug: "",
-    content: {
-      title: "",
-      summary: "",
-      body: "",
-    },
-    coverImage: "",
-    tags: [],
-    photographer: undefined,
-    achievements: [],
-    blogType: "blog",
-  });
-
-  const validateForm = () => {
-    if (!blog.content.title.trim()) {
-      toast.error("Title is required.");
-      return false;
-    }
-    if (!blog.content.body.trim()) {
-      toast.error("Body is required.");
-      return false;
-    }
-    return true;
-  };
-
-  const handleImageUpload = async (e) => {
-    const file = e.target.files[0];
-    if (!file) return;
-
-    const uploadData = new FormData();
-    uploadData.append("image", file);
-
-    try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_SERVER}/api/upload/uploadSingleImage`,
-        {
-          method: "POST",
-          body: uploadData,
+              headers: { "x-auth-token": token },
+            }
+          );
+          toast.success("Blog deleted successfully!");
+          setBlogs((prev) => prev.filter((blog) => blog._id !== id));
+        } catch (error) {
+          console.error("Error deleting blog:", error);
+          toast.error("Failed to delete blog.");
         }
-      );
-      const data = await res.text();
-      if (res.ok) {
-        setBlog((prev) => ({
-          ...prev,
-          coverImage: data,
-        }));
-        toast.success("Image uploaded successfully!");
-      } else {
-        toast.error("Image upload failed.");
       }
-    } catch (error) {
-      console.error(error);
-      toast.error("An error occurred while uploading the image.");
-    }
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!validateForm()) return;
-    try {
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_SERVER}/api/blog/add-blog`,
-        blog,
-        {
-          headers: {
-            "x-auth-token": token,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = await response.data;
-      if (data.error) {
-        return toast.error(data.error);
-      }
-      //blog sent for approval
-      Swal.fire({
-        title: "Blog sent for approval",
-        text: "Your blog has been sent for approval. You will be notified once it is approved.",
-        icon: "success",
-        confirmButtonText: "OK",
-      }).then(() => {
-        router.push(`/profile`);
-      });
-    } catch (error) {
-      console.error(error);
-      setError(error.response.data.message);
-    }
+    });
   };
 
   useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_SERVER}/api/blog/get-my-blogs?author=${photographer._id}`
+        );
+        const data = response.data;
+
+        setBlogs(data.blogs);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     if (photographer) {
-      setBlog((prev) => ({
-        ...prev,
-        authorInfo: { author: photographer._id, authorType: "Photographer" },
-        photographer: photographer._id,
-      }));
+      fetchBlogs();
     }
   }, [photographer]);
 
-  console.log(blog);
-
   return (
-    <div className="p-4">
+    <div className="p-4 min-h-screen">
       {photographer ? (
-        <form
-          onSubmit={handleSubmit}
-          className="max-w-4xl mx-auto mt-5 min-h-screen"
-        >
+        <div className="">
           {/* <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">
             Create Blog
           </h1> */}
-          <div className="bg-white shadow-[0_0_8px_rgba(0,0,0,0.3)] rounded-lg p-6 space-y-6">
-            <div>
-              <Label htmlFor="coverImage">Cover Image</Label>
-              <Input
-                id="coverImage"
-                type="file"
-                accept="image/*"
-                onChange={handleImageUpload}
-              />
-              {blog.coverImage && (
-                <img
-                  src={blog.coverImage}
-                  alt="Cover Preview"
-                  className="mt-4 w-full h-48 object-contain rounded-lg shadow-sm"
-                />
-              )}
+          <div className="flex flex-col flex-1 gap-4 p-4">
+            <div className="flex items-center justify-between">
+              <h2 className="text-lg font-semibold">All Blogs</h2>
+              <Link href="/profile/blog/create">
+                <Button2 size="sm">Create Blog</Button2>
+              </Link>
             </div>
-            <div>
-              <Label htmlFor="title">Title</Label>
-              <Input
-                id="title"
-                type="text"
-                value={blog.content.title}
-                onChange={(e) =>
-                  setBlog({
-                    ...blog,
-                    content: { ...blog.content, title: e.target.value },
-                  })
-                }
-              />
-            </div>
-            <div>
-              <Label htmlFor="summary">Summary</Label>
-              <Textarea
-                id="summary"
-                value={blog.content.summary}
-                onChange={(e) =>
-                  setBlog({
-                    ...blog,
-                    content: { ...blog.content, summary: e.target.value },
-                  })
-                }
-              />
-            </div>
-            <div>
-              <Label htmlFor="body">Body</Label>
-              <ReactQuill
-                modules={modules}
-                theme="snow"
-                value={blog.content.body}
-                onChange={(value) =>
-                  setBlog({
-                    ...blog,
-                    content: { ...blog.content, body: value },
-                  })
-                }
-              />
-            </div>
-            <div>
-              <Label htmlFor="tags">Tags</Label>
-              <Input
-                id="tags"
-                type="text"
-                value={blog.tags.join(", ")}
-                onChange={(e) =>
-                  setBlog({
-                    ...blog,
-                    tags: e.target.value.split(",").map((tag) => tag.trim()),
-                  })
-                }
-              />
-            </div>
-            <hr />
-            {error && <p className="text-red-500 text-sm">{error}</p>}
-            <Button
-              variant="primary"
-              type="submit"
-              className="w-full bg-blue-600 text-white hover:bg-blue-700"
-            >
-              Create Blog
-            </Button>
+
+            {isLoading ? (
+              <div className="flex items-center justify-center h-96">
+                <Loader />
+              </div>
+            ) : blogs.length === 0 ? (
+              <div className="flex items-center justify-center h-96">
+                <h1 className="text-xl text-gray-500">No blogs found</h1>
+              </div>
+            ) : (
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+                {blogs.map((blog) => (
+                  <div
+                    key={blog._id}
+                    className="flex flex-col justify-between gap-4 p-4 bg-white rounded-lg shadow-md"
+                  >
+                    <div className="flex flex-col gap-4">
+                      {/* Blog Image */}
+                      <img
+                        src={blog.coverImage}
+                        alt={blog.content.title}
+                        className="w-full aspect-[16/9] object-cover rounded-lg"
+                      />
+
+                      {/* Blog Details */}
+                      <div className="flex flex-wrap gap-2 items-center">
+                        {blog.tags.slice(0, 3).map((tag) => (
+                          <span
+                            key={tag}
+                            className="px-2 py-1 bg-blue-100 text-blue-600 rounded-full text-sm font-semibold"
+                          >
+                            {tag}
+                          </span>
+                        ))}
+                        <span className="text-sm text-gray-600 font-semibold">
+                          {blog.tags.length > 3 &&
+                            `+${blog.tags.length - 3} more`}
+                        </span>
+                      </div>
+                      <h2 className="text-lg font-semibold">
+                        {blog.content.title}
+                      </h2>
+                      <p className="text-sm text-gray-700 truncate">
+                        {blog.content.summary}
+                      </p>
+                      <p className="text-sm text-gray-600">
+                        {new Date(blog.createdAt).toLocaleDateString()}
+                      </p>
+                    </div>
+                    {/* Action Buttons */}
+                    <div className="flex items-center justify-between mt-4">
+                      <Link href={`/blog/${blog._id}`}>
+                        <Button2 size="sm">Read More</Button2>
+                      </Link>
+                      <div className="flex gap-2">
+                        <Link href={`/profile/blog/edit/${blog._id}`}>
+                          <Button
+                            variant="outline"
+                            className="flex items-center gap-2"
+                          >
+                            {/* <Pen size={16} /> */}
+                            Edit
+                          </Button>
+                        </Link>
+                        <Button
+                          variant="destructive"
+                          className="flex items-center gap-2"
+                          onClick={() => handleDelete(blog._id)}
+                        >
+                          <Trash size={16} />
+                          Delete
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-        </form>
+        </div>
       ) : (
-        <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
+        <div className="flex flex-col items-center justify-center h-96 space-y-4">
           {isHydrated ? (
             <h1 className="text-3xl font-bold text-center text-gray-800">
               You need to be a photographer to create a blog.
