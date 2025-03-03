8640b2a3ee057d913756ee863633429d567a2122
bhanu-sh <bhanu1234sharma@gmail.com>
2025-03-03 16:44:00 +0530

    refactor: integrate framer-motion for enhanced animations and layout transitions

--- app/page.jsx
+++ app/page.jsx
@@ -12,6 +12,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import DiscoverMobileLoader from "@/components/home/discoverMobileLoader";
 import DiscoverLoader from "@/components/home/discoverLoader";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Home() {
   const [stories, setStories] = useState([]);
@@ -55,7 +56,7 @@ export default function Home() {
       {/* <div className="px-4 sm:px-32 mt-28 mb-0"> */}
       {/* <p className="text-paragraph sm:text-heading-05 font-semibold text-surface-600 text-justify">
             <span className="text-heading-04 sm:text-heading-03 font-bold">
-              ClickedART{" "}
+            ClickedART{" "}
             </span>
             offers an exclusive collection of large-format photographic wall
             art, featuring breathtaking, ultra-high-resolution images by
@@ -64,22 +65,18 @@ export default function Home() {
             dedicated to empowering Indian photographers, providing a
             professional space for them to showcase and sell their exceptional
             work.
-          </p> */}
+            </p> */}
       {/* <Link href={"/explore"} className="pt-10">
             <Button size="xl" variant="filled" state="default">
-              Know more about us
+            Know more about us
             </Button>
-          </Link> */}
+            </Link> */}
       {/* </div> */}
       <div className="px-4 my-5">
         <ProductCategoriesMobile />
       </div>
       <div className="hidden sm:block px-4 mb-5">
-      {storyLoading ? (
-        <DiscoverLoader />
-      ) : (
-          <Discover stories={stories} />
-        )}
+        {storyLoading ? <DiscoverLoader /> : <Discover stories={stories} />}
       </div>
       <div className="block sm:hidden px-6 mb-5">
         {storyLoading ? (
@@ -91,11 +88,15 @@ export default function Home() {
       {/* <div className=" px-4 mb-5">
         <BestSelling />
       </div> */}
-      {/* <div className="px-4">
+      <div className="px-4">
+      <motion.div layout>
         <Testimonial />
-      </div> */}
+        </motion.div>
+      </div>
       <div className="mt-5">
-        <Stats layout={layout} />
+        <motion.div layout>
+          <Stats layout={layout} />
+        </motion.div>
       </div>
     </div>
   );
--- components/footer.jsx
+++ components/footer.jsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import useLayoutStore from "@/store/layout";
 import Swal from "sweetalert2";
 import useAuthStore from "@/authStore";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Footer() {
   const { user, photographer } = useAuthStore();
@@ -22,141 +23,146 @@ export default function Footer() {
   )
     return null;
   return (
-    <div className={`bg-primary-100 pb-5 px-4 sm:px-10 md:px-24 z-50`}>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 py-5 gap-y-5 gap-x-10 w-full">
-        <div className="flex flex-col gap-2 w-full">
-          <Image
-            width={200}
-            height={100}
-            onClick={() => {
-              router.push("/");
-            }}
-            src={layout?.logo || "/assets/Logo.png"}
-            alt="ClickedArt.com"
-            className="w-80"
-          />
-          {layout?.footerDetails?.phone ? (
-            <p className="mt-5">{layout?.footerDetails?.phone}</p>
-          ) : (
-            <div className="mt-5 animate-pulse w-24 h-5 bg-surface-300 rounded-md"></div>
-          )}
-          {layout?.footerDetails?.email ? (
-            <p>{layout?.footerDetails?.email}</p>
-          ) : (
-            <div className="animate-pulse w-24 h-5 bg-surface-300 rounded-md"></div>
-          )}
-          <div className="flex gap-4 text-sm underline underline-offset-1">
-            <Link href="/terms">Terms and Conditions</Link>
-            <Link href="/privacy-policy">Privacy Policy</Link>
-          </div>
-        </div>
-        <div className="grid grid-cols-3 w-full">
-          <div className="flex flex-col gap-2">
-            <p className="font-semibold text-lg">Follow us</p>
-            <div className="mt-3 flex flex-col gap-2">
-              {layout ? (
-                <>
-                  {layout.footerDetails?.footerlinks?.map((link, index) => {
-                    const formattedLink =
-                      link.accountLink?.startsWith("http://") ||
-                      link.accountLink?.startsWith("https://")
-                        ? link.accountLink
-                        : `https://${link.accountLink}`;
-
-                    return (
-                      <Link
-                        key={index}
-                        href={formattedLink || "/"}
-                        className="text-sm font-medium"
-                        target="_blank" // Optional: Opens the link in a new tab
-                        rel="noopener noreferrer" // Security best practice for external links
-                      >
-                        {link.accountName}
-                      </Link>
-                    );
-                  })}
-                </>
-              ) : (
-                <>
-                  {[...Array(4).keys()].map((index) => (
-                    <div
-                      key={index}
-                      className="animate-pulse w-24 h-5 bg-surface-300 rounded-md"
-                    ></div>
-                  ))}
-                </>
-              )}
+    <AnimatePresence>
+      <motion.div layout className={`bg-primary-100 pb-5 px-4 sm:px-10 md:px-24 z-50`}>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 py-5 gap-y-5 gap-x-10 w-full">
+          <div className="flex flex-col gap-2 w-full">
+            <Image
+              width={200}
+              height={100}
+              onClick={() => {
+                router.push("/");
+              }}
+              src={layout?.logo || "/assets/Logo.png"}
+              alt="ClickedArt.com"
+              className="w-80"
+            />
+            {layout?.footerDetails?.phone ? (
+              <p className="mt-5">{layout?.footerDetails?.phone}</p>
+            ) : (
+              <div className="mt-5 animate-pulse w-24 h-5 bg-surface-300 rounded-md"></div>
+            )}
+            {layout?.footerDetails?.email ? (
+              <p>{layout?.footerDetails?.email}</p>
+            ) : (
+              <div className="animate-pulse w-24 h-5 bg-surface-300 rounded-md"></div>
+            )}
+            <div className="flex gap-4 text-sm underline underline-offset-1">
+              <Link href="/terms">Terms and Conditions</Link>
+              <Link href="/privacy-policy">Privacy Policy</Link>
             </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <p className="font-semibold text-lg">Explore</p>
-            <Link href="/" className="mt-3 text-sm font-medium">
-              Home
-            </Link>
-            <Link href="/images" className="text-sm font-medium">
-              Images
-            </Link>
-            <Link href="/explore" className="text-sm font-medium">
-              Explore
-            </Link>
-            <Link href="/explore?contact" className="text-sm font-medium">
-              Contact us
-            </Link>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p className="font-semibold text-lg">Support</p>
-            <Link
-              href="/support/bulk-download"
-              className="mt-3 text-sm font-medium"
-            >
-              Bulk Download
-            </Link>
-            <Link
-              href="/support/custom-enquiry"
-              className="text-sm font-medium"
-            >
-              Custom Enquiry
-            </Link>
-            <Link href="/verify" className="text-sm font-medium">
-              Verify Email
-            </Link>
-            <Link href="/explore#supportVideo" className="text-sm font-medium">
-              Support Videos
-            </Link>
-          </div>
-        </div>
-        <div className="flex flex-col gap-2 items-start">
-          <p className="font-semibold text-lg">
-            Sell your photographs on ClickedArt!
-          </p>
-          <p className="mt-3 text-sm font-medium">
-            Turn Your Passion into Profit — Join Our Community of Creators
-          </p>
-          <div>
-            <Button
-              onClick={() => {
-                if (photographer) router.push("/profile");
-                else if (user)
-                  Swal.fire(
-                    "Please sign in as a Photographer to sell your photos"
-                  );
-                else router.push("/signup/photographer");
-              }}
-              size="base"
-              variant="filled"
-              state="default"
-            >
-              <p className="font-normal">Start Selling Now</p>
-            </Button>
-          </div>
-        </div>
-      </div>
+          <div className="grid grid-cols-3 w-full">
+            <div className="flex flex-col gap-2">
+              <p className="font-semibold text-lg">Follow us</p>
+              <div className="mt-3 flex flex-col gap-2">
+                {layout ? (
+                  <>
+                    {layout.footerDetails?.footerlinks?.map((link, index) => {
+                      const formattedLink =
+                        link.accountLink?.startsWith("http://") ||
+                        link.accountLink?.startsWith("https://")
+                          ? link.accountLink
+                          : `https://${link.accountLink}`;
 
-      <hr className="border-surface-300" />
-      <div className="h-2"></div>
-      <p className="text-sm font-medium">
-        ©2025 ClickedArt® is a registered trademark.
-      </p>
-    </div>
+                      return (
+                        <Link
+                          key={index}
+                          href={formattedLink || "/"}
+                          className="text-sm font-medium"
+                          target="_blank" // Optional: Opens the link in a new tab
+                          rel="noopener noreferrer" // Security best practice for external links
+                        >
+                          {link.accountName}
+                        </Link>
+                      );
+                    })}
+                  </>
+                ) : (
+                  <>
+                    {[...Array(4).keys()].map((index) => (
+                      <div
+                        key={index}
+                        className="animate-pulse w-24 h-5 bg-surface-300 rounded-md"
+                      ></div>
+                    ))}
+                  </>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="font-semibold text-lg">Explore</p>
+              <Link href="/" className="mt-3 text-sm font-medium">
+                Home
+              </Link>
+              <Link href="/images" className="text-sm font-medium">
+                Images
+              </Link>
+              <Link href="/explore" className="text-sm font-medium">
+                Explore
+              </Link>
+              <Link href="/explore?contact" className="text-sm font-medium">
+                Contact us
+              </Link>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="font-semibold text-lg">Support</p>
+              <Link
+                href="/support/bulk-download"
+                className="mt-3 text-sm font-medium"
+              >
+                Bulk Download
+              </Link>
+              <Link
+                href="/support/custom-enquiry"
+                className="text-sm font-medium"
+              >
+                Custom Enquiry
+              </Link>
+              <Link href="/verify" className="text-sm font-medium">
+                Verify Email
+              </Link>
+              <Link
+                href="/explore#supportVideo"
+                className="text-sm font-medium"
+              >
+                Support Videos
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 items-start">
+            <p className="font-semibold text-lg">
+              Sell your photographs on ClickedArt!
+            </p>
+            <p className="mt-3 text-sm font-medium">
+              Turn Your Passion into Profit — Join Our Community of Creators
+            </p>
+            <div>
+              <Button
+                onClick={() => {
+                  if (photographer) router.push("/profile");
+                  else if (user)
+                    Swal.fire(
+                      "Please sign in as a Photographer to sell your photos"
+                    );
+                  else router.push("/signup/photographer");
+                }}
+                size="base"
+                variant="filled"
+                state="default"
+              >
+                <p className="font-normal">Start Selling Now</p>
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        <hr className="border-surface-300" />
+        <div className="h-2"></div>
+        <p className="text-sm font-medium">
+          ©2025 ClickedArt® is a registered trademark.
+        </p>
+      </motion.div>
+    </AnimatePresence>
   );
 }
